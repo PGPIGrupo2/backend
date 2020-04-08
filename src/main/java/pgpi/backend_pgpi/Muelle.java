266bf813f0bb389b635c4ed2825da51909a61cbf
atimermans <atimermansp@gmail.com>
2020-04-08 17:14:56 +0200

    BUG: set time zone, move file

--- src/main/java/pgpi/backend_pgpi/Muelle.java
+++ src/main/java/pgpi/backend_pgpi/Muelle.java
@@ -1,14 +1,11 @@
 package pgpi.backend_pgpi;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Muelle {
