4360f2e5982c0b439db3b58657bd889cca078e78
atimermans <atimermansp@gmail.com>
2020-04-10 09:20:10 +0200

    BUG: id

--- src/main/java/pgpi/backend_pgpi/PropuestaReserva.java
+++ src/main/java/pgpi/backend_pgpi/PropuestaReserva.java
@@ -1,6 +1,9 @@
 package pgpi.backend_pgpi;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class PropuestaReserva {
@@ -9,6 +12,9 @@ public class PropuestaReserva {
 		this.muelleId = muelleId;
 		this.hora = hora;
 	}
+	  @Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	  private Integer id;
 	private int muelleId;
 	private int hora;
 	public int getMuelleId() {
